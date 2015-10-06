d675979d75339cfc438e7ac5bec87a87aa18118e
Oliver Lorenz <mail@oliverlorenz.com>
2015-10-06 16:14:40 +0200

    save

--- examples/connectSubscribe.php
+++ examples/connectSubscribe.php
@@ -15,6 +15,10 @@ $connector = new oliverlorenz\reactphpmqtt\Connector($loop, $resolver, $version)
 $connector->create($config['server'], 1883);
 $connector->onConnected(function() use ($connector) {
     $connector->subscribe('#', 0);
+
+    $connector->onPublishReceived(function ($message) {
+        print_r($message);
+    });
 });
 
 $loop->run();
