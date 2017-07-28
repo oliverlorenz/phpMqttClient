82f617825d15f2d63431d0d702f7f1e5d578ea6e
Jonathan Jefferies <jonathan@jjok.co.uk>
2017-07-28 19:18:50 +0100

    Add test for `ClientFactory`

---
+++ tests/unit/ClientFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace oliverlorenz\reactphpmqtt;
+
+use oliverlorenz\reactphpmqtt\protocol\Version4;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @covers \oliverlorenz\reactphpmqtt\ClientFactory
+ */
+final class ClientFactoryTest extends TestCase
+{
+    public function testConnectorCanBeCreated()
+    {
+        $client = ClientFactory::createClient(new Version4(), '8.8.8.8');
+
+        $this->assertInstanceOf('oliverlorenz\reactphpmqtt\Connector', $client);
+    }
+
+    public function testSecureConnectorCanBeCreated()
+    {
+        $client = ClientFactory::createSecureClient(new Version4(), '8.8.8.8');
+
+        $this->assertInstanceOf('oliverlorenz\reactphpmqtt\SecureConnector', $client);
+    }
+}
